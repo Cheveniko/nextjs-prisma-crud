58b4c962ca4aca34931774e6c1545e2ee16b0cc9
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2023-09-02 00:02:57 -0500

    Upload files

---
+++ src/app/about/page.jsx
@@ -0,0 +1,22 @@
+function AboutPage() {
+  return (
+    <section className="container mx-auto">
+      <h3 className="text-4xl font-bold my-5">About</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, corporis
+        earum atque necessitatibus dolorem labore non delectus repudiandae
+        quidem cum autem harum officiis! Autem minus culpa, dolorem porro
+        adipisci dolorum, distinctio eos in ipsum excepturi vero repellendus
+        numquam aperiam nam repellat sapiente facere debitis quia ipsam natus
+        ullam! Culpa illo suscipit rem nihil ratione perferendis, obcaecati
+        nulla saepe ducimus eos, cupiditate et ipsum id sit magni minus
+        veritatis illum totam! Explicabo ipsam quam, optio quidem, similique
+        pariatur doloremque debitis tenetur fugit eaque facilis provident sed ut
+        quos! Possimus voluptatibus ex consectetur expedita voluptatem
+        exercitationem quod itaque quae? Aliquam, rerum libero!
+      </p>
+    </section>
+  );
+}
+
+export default AboutPage;
---
+++ src/app/new/page.jsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+function NewPage({ params }) {
+  const router = useRouter();
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    if (params.id) {
+      fetch(`/api/tasks/${params.id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setTitle(data.title);
+          setDescription(data.description);
+        });
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (params.id) {
+      const res = await fetch(`/api/tasks/${params.id}`, {
+        method: "PUT",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+    } else {
+      const res = await fetch("/api/tasks", {
+        method: "POST",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+    }
+    router.refresh();
+    router.push("/");
+  };
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <form
+        className="bg-slate-800 p-10 lg:w-1/4 md:w-1/2"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="title" className="font-bold text-sm">
+          Título de la tarea
+        </label>
+        <input
+          type="text"
+          id="title"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          placeholder="Título"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label htmlFor="description" className="font-bold text-sm">
+          Descripción de la tarea
+        </label>
+        <textarea
+          rows="3"
+          id="description"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          placeholder="Describe tu tarea"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        ></textarea>
+        <div className="flex justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            {params.id ? "Editar" : "Crear"}
+          </button>
+          {params.id && (
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-4"
+              type="button"
+              onClick={async () => {
+                const res = await fetch(`/api/tasks/${params.id}`, {
+                  method: "DELETE",
+                });
+                const data = res.json();
+                router.refresh();
+                router.push("/");
+              }}
+            >
+              Borrar
+            </button>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default NewPage;
